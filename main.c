222e0a5ae690d2a7efd1a74682cbd0b305aeff30
Muhammad Hafidz <24hafidz@mhs.usk.ac.id>
2024-11-20 11:38:33 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 int main() {
     int registerChoice;
-    char Y,n;
 
     printf("========================================\n");
     printf("         🌟 Welcome To Quiz Game 🌟     \n");
     printf("        WHO WANTS TO BE A MILLIONARE      \n");
     printf("========================================\n");
-    printf("Do you want to Register? (Y/n): ");
+    printf("Do you want to Register? (1 for Yes, 0 for no): ");
     scanf("%d", &registerChoice);
 
-    if (registerChoice == Y) {
+    if (registerChoice == 1) {
         signUp();
         signIn();
     } else {
